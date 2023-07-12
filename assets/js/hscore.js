521a89495227cf4185db2006abb4804bd97b5e11
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2023-07-11 21:10:43 -0500

    add index.html

---
+++ assets/js/hscore.js
@@ -0,0 +1,21 @@
+var scoresBtn = document.querySelector("#view-high-scores");
+
+// Rank previous scores in order by retrieving scores from localStorage
+
+function printHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    highscores.forEach(function(score) {
+      var liTag = document.createElement("li");
+      liTag.textContent = score.name + " - - - - - " + score.score;
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+}
+
+// Clear previous scores when users click clear 
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  } document.getElementById("clear").onclick = clearHighscores;
+  
+printHighscores();
